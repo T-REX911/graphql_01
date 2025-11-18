92a303688aca485512e785427a574b69ff662cc0
T-REX911 <163975890+T-REX911@users.noreply.github.com>
2025-11-18 22:26:46 +0530

    initial commit

---
+++ src/main/java/com/trex/graphql_01/Graphql01Application.java
@@ -0,0 +1,13 @@
+package com.trex.graphql_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Graphql01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Graphql01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/trex/graphql_01/Graphql01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trex.graphql_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Graphql01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
